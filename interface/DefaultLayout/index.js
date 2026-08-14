6ea15737dd995fca1b9b94192c9ffb15211b5dde
Alan Miranda <alannub6@gmail.com>
2026-08-14 01:59:10 +0000

    feat: add `contentWidth` suport to `DefaultLayout`

--- interface/DefaultLayout/index.js
+++ interface/DefaultLayout/index.js
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import { PageLayout, Header, Text } from "@primer/react";
+import styles from "./index.module.css";
 
-export default function DefaultLayout({ children, metadata = {} }) {
+const contentWidthClasses = {
+  small: styles.smallContent
+};
+
+export default function DefaultLayout({ children, metadata = {}, contentWidth }) {
+  
+  const extraContentClassName = contentWidthClasses[contentWidth];
+
   return (
     <>
       <Head>
@@ -13,18 +21,18 @@ export default function DefaultLayout({ children, metadata = {} }) {
         )}
       </Head>
       <Header>
-            <Header.Item full>
-              <Header.Link href="/">FinTab</Header.Link>
-            </Header.Item>
-            <Header.Item>
-              <Header.Link href="/login">Login</Header.Link>
-            </Header.Item>
-            <Header.Item>
-              <Header.Link href="/cadastro">Cadastrar</Header.Link>
-            </Header.Item>
-          </Header>
+        <Header.Item full>
+          <Header.Link href="/">FinTab</Header.Link>
+        </Header.Item>
+        <Header.Item>
+          <Header.Link href="/login">Login</Header.Link>
+        </Header.Item>
+        <Header.Item>
+          <Header.Link href="/cadastro">Cadastrar</Header.Link>
+        </Header.Item>
+      </Header>
       <PageLayout>
-        <PageLayout.Content>
+        <PageLayout.Content width={contentWidth} className={extraContentClassName}>
           {children}
         </PageLayout.Content>
         <PageLayout.Footer divider="line">
